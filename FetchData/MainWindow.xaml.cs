055b20b62ac9e5aaa0f518efb0f63414ef431c52
Duc Tung <hbtthangtien@gmail.com>
2025-01-17 12:01:02 +0700

    Build project Fetch Data from a URL Using HttpClient

--- FetchData/MainWindow.xaml.cs
+++ FetchData/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System.Net.Http;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -16,9 +17,32 @@ namespace FetchData
     /// </summary>
     public partial class MainWindow : Window
     {
+        private readonly HttpClient httpClient = new HttpClient();
         public MainWindow()
         {
             InitializeComponent();
         }
+
+        private async void btnViewHTML_Click(object sender, RoutedEventArgs e)
+        {
+            string uri = txtURL.Text;
+            try
+            {
+                string responseBody = await httpClient.GetStringAsync(uri);
+                txtContent.Text = responseBody.Trim();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Message: {ex.Message}");
+            }
+        }
+
+        private void btnClear_Click(object sender, RoutedEventArgs e)
+        {
+            txtURL.Clear();
+            txtContent.Clear();
+        }
+
+        private void btnCloese_Click(object sender, RoutedEventArgs e) => Close();
     }
 }
